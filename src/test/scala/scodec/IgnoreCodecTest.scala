6ce5e87ef4c59236c2d5c7579c6a127633be97bf
Michael Pilquist <mpilquist@gmail.com>
2013-04-23 12:55:08 -0400

    Removing unnecessary uint2 codec from IgnoreCodecTest

--- src/test/scala/scodec/IgnoreCodecTest.scala
+++ src/test/scala/scodec/IgnoreCodecTest.scala
@@ -8,7 +8,6 @@ import Codecs._
 class IgnoreCodecTest extends CodecSuite {
 
   test("roundtrip") {
-    val uint2 = new IntCodec(2, signed = false)
     val codec = Codecs.ignore(2) ~> uint4 <~ Codecs.ignore(2)
     codec.decode(BitVector(0xff)) shouldBe \/-((BitVector.empty, 15))
     codec.encode(15) shouldBe \/-(BitVector(0x3c))
