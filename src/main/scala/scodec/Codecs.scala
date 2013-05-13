43440df3f5fd2f8eea218857a2aff9764203f396
Michael Pilquist <mpilquist@gmail.com>
2013-05-12 23:35:47 -0400

    Signature support

--- src/main/scala/scodec/Codecs.scala
+++ src/main/scala/scodec/Codecs.scala
@@ -71,6 +71,7 @@ object Codecs extends NamedCodecSyntax with TupleCodecSyntax with HListCodecSynt
   def repeated[A](codec: Codec[A]): Codec[collection.immutable.IndexedSeq[A]] = new IndexedSeqCodec(codec)
 
   def encrypted[A](codec: Codec[A])(implicit cipherFactory: CipherFactory): Codec[A] = new CipherCodec(codec)(cipherFactory)
+  def signed[A](codec: Codec[A])(implicit signatureFactory: SignatureFactory): Codec[A] = new SignatureCodec(codec)(signatureFactory)
 
   // Needed for the ignore combinator when used with <~, ~>, and :~>:
   implicit val unitInstance = scalaz.std.anyVal.unitInstance
