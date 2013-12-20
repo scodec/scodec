d395a291d0577f7819261e1098915b55268fae02
Paul Chiusano <paul.chiusano@gmail.com>
2013-12-20 10:11:41 -0500

    Fixed up error message in VariableSizeCodec, all tests now passing

--- src/main/scala/scodec/VariableSizeCodec.scala
+++ src/main/scala/scodec/VariableSizeCodec.scala
@@ -9,12 +9,12 @@ class VariableSizeCodec[A](sizeCodec: Codec[Int], valueCodec: Codec[A]) extends 
 
   override def encode(a: A) = for {
     encA <- valueCodec.encode(a)
-    encSize <- encA.intSize.map(n => sizeCodec.encode(n).leftMap { fail(a, n, _) })
-                           .getOrElse(left(fail(a, encA.size, "")))
+    encSize <- encA.intSize.map(n => sizeCodec.encode(n).leftMap { fail(a, _) })
+                           .getOrElse(left(fail(a, s"${encA.size} exceeds maximum 32-bit integer value ${Int.MaxValue}")))
   } yield encSize ++ encA
 
-  private def fail(a: A, n: Long, msg: String): String =
-    s"[$a] is too long to be encoded: $n $msg"
+  private def fail(a: A, msg: String): String =
+    s"[$a] is too long to be encoded: $msg"
 
   override def decode(buffer: BitVector) =
     decoder.decode(buffer).map { case (rest, (sz, value)) => (rest, value) }
