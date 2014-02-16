b7361828c49ddac3ee861a0d9da4e82ff9dfbe7b
Michael Pilquist <mpilquist@gmail.com>
2014-02-15 22:19:55 -0500

    Remove aliases for BitVector and ByteVector

--- src/main/scala/scodec/codecs/DoubleCodec.scala
+++ src/main/scala/scodec/codecs/DoubleCodec.scala
@@ -5,7 +5,7 @@ import java.nio.{ ByteBuffer, ByteOrder }
 
 import scalaz.syntax.id._
 import scalaz.syntax.std.either._
-import scodec.bits.BitVector
+import scodec.bits.{ BitVector, ByteVector }
 
 private[codecs] final class DoubleCodec(bigEndian: Boolean) extends Codec[Double] {
 
