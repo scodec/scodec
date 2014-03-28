c586e96e90fe5134c62df04bcfb9a20ca62e0992
Michael Pilquist <mpilquist@gmail.com>
2014-03-28 11:04:21 -0400

    Simplify UDP example

--- src/test/scala/scodec/examples/UdpDatagramExample.scala
+++ src/test/scala/scodec/examples/UdpDatagramExample.scala
@@ -10,7 +10,6 @@ class UdpDatagramExample extends CodecSuite {
   case class Datagram(
     sourcePort: Int,
     destinationPort: Int,
-    length: Int,
     checksum: Int,
     data: ByteVector)
   implicit val datagramIso = Iso.hlist(Datagram.apply _, Datagram.unapply _)
@@ -19,17 +18,17 @@ class UdpDatagramExample extends CodecSuite {
     import scodec.codecs._
 
     implicit val datagram = {
-      ("source_port" | uint16        ) ::
-      ("dest_port"   | uint16        ) ::
-      ("length"      | uint16        ).>>:~ { length =>
-      ("checksum"    | uint16        ) ::
-      ("data"        | bytes(length) )
-    }}.as[Datagram]
+      ("source_port" | uint16   ) ::
+      ("dest_port"   | uint16   ) ::
+      variableSizeBytes(uint16,
+      ("checksum"    | uint16   ) ::
+      ("data"        | bytes    ))
+    }.as[Datagram]
   }
 
   import UdpCodec._
 
   test("roundtrip") {
-    roundtrip(Datagram(1234, 2345, 100, 0, BitVector.high(100 * 8).toByteVector))
+    roundtrip(Datagram(1234, 2345, 0, ByteVector.fill(0xff)(100)))
   }
 }
