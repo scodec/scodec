65934a67097cc69d6e685775b328cd772ccd94e2
Richard Searle <harold.richard.searle@gmail.com>
2014-09-11 21:38:50 -0500

    adjust per the review feedback

--- src/test/scala/scodec/CodecTest.scala
+++ src/test/scala/scodec/CodecTest.scala
@@ -1,6 +1,6 @@
 package scodec
 
-import scalaz.\/
+import scalaz.{\/, -\/, \/-}
 
 import scodec.bits._
 import scodec.codecs._
@@ -65,6 +65,47 @@ class CodecTest extends CodecSuite {
       (1.toByte ~> uint8).encode(2) shouldBe \/.right(hex"0102".bits)
       (hex"11223344" ~> uint8).encode(2) shouldBe \/.right(hex"1122334402".bits)
       (hex"11223344".bits ~> uint8).encode(2) shouldBe \/.right(hex"1122334402".bits)
+    }
+  }
+  
+    "exmap" should {
+    "accept only values no greater than 9" which {
+
+      // accept 8 bit values no greater than 9
+      val oneDigit: Codec[Int] = uint8.exmap[Int](
+        v => if (v > 9) -\/("badv") else \/-(v),
+        d => if (d > 9) -\/("badd") else \/-(d))
+
+      "3 is accepted" in {
+        oneDigit.encode(3) shouldBe \/-(BitVector(0x03))
+      }
+      "10 is rejected" in {
+        oneDigit.encode(10) shouldBe -\/("badd")
+      }
+      "too large to fit in 8 bits" in {
+        oneDigit.encode(30000000) shouldBe -\/("badd")
+      }
+      "0x05 is acceptable" in {
+        oneDigit.decode(BitVector(0x05)) shouldBe \/-((BitVector.empty, 5))
+      }
+      "0xff is rejected" in {
+        oneDigit.decode(BitVector(0xff)) shouldBe -\/("badv")
+      }
+      "empty BitVector is rejected" in {
+        oneDigit.decode(BitVector.empty) shouldBe uint8.decode(BitVector.empty)
+      }
+    }
+    "acccept any 8 bit value" which {
+      // empty implementation that accepts all values
+      val noop: Codec[Int] = uint8.exmap[Int](v => \/-(v),
+        d => \/-(d))
+
+      "value that fits in 8 bits" in {
+        noop.encode(10) shouldBe \/-(BitVector(0x0a))
+      }
+      "value that exceeds 8 bits" in {
+        noop.encode(30000000) shouldBe uint8.encode(30000000)
+      }
     }
   }
 }
