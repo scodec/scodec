94e82827216e0d7907caf7dd5c9ffc6e01d6ef8f
Michael Pilquist <mpilquist@gmail.com>
2014-09-29 21:57:30 -0400

    Add short codecs

---
+++ src/main/scala/scodec/codecs/ShortCodec.scala
@@ -0,0 +1,38 @@
+package scodec
+package codecs
+
+import scalaz.\/
+import scalaz.syntax.std.either._
+import scalaz.syntax.std.option._
+
+import java.nio.{ ByteBuffer, ByteOrder }
+
+import scodec.bits.{ BitVector, ByteOrdering, ByteVector }
+
+private[codecs] final class ShortCodec(bits: Int, signed: Boolean, ordering: ByteOrdering) extends Codec[Short] {
+
+  require(bits > 0 && bits <= (if (signed) 16 else 15), "bits must be in range [1, 16] for signed and [1, 15] for unsigned")
+
+  val MaxValue = ((1 << (if (signed) (bits - 1) else bits)) - 1).toShort
+  val MinValue = (if (signed) -(1 << (bits - 1)) else 0).toShort
+
+  private def description = s"$bits-bit ${if (signed) "signed" else "unsigned"} short"
+
+  override def encode(s: Short) = {
+    if (s > MaxValue) {
+      \/.left(s"$s is greater than maximum value $MaxValue for $description")
+    } else if (s < MinValue) {
+      \/.left(s"$s is less than minimum value $MinValue for $description")
+    } else {
+      \/.right(BitVector.fromShort(s, bits, ordering))
+    }
+  }
+
+  override def decode(buffer: BitVector) =
+    buffer.acquire(bits) match {
+      case Left(e) => \/.left(e)
+      case Right(b) => \/.right((buffer.drop(bits), b.toShort(signed, ordering)))
+    }
+
+  override def toString = description
+}
---
+++ src/test/scala/scodec/codecs/ShortCodecTest.scala
@@ -0,0 +1,48 @@
+package scodec
+package codecs
+
+import scalaz.\/
+import scalaz.syntax.std.option._
+import org.scalacheck.Gen
+import scodec.bits.BitVector
+
+class ShortCodecTest extends CodecSuite {
+  def check(low: Short, high: Short)(f: (Short) => Unit) {
+    forAll(Gen.choose(low, high)) { n =>
+      whenever(n >= low) { f(n) }
+    }
+  }
+
+  "the short16 codec" should { "roundtrip" in { forAll { (n: Short) => roundtrip(short16, n) } } }
+  "the short16L codec" should { "roundtrip" in { forAll { (n: Short) => roundtrip(short16L, n) } } }
+  "the ushort(n) codec" should { "roundtrip" in { forAll { (n: Short) => whenever(n >= 0) { roundtrip(ushort(15), n) } } } }
+  "the ushortL(n) codec" should { "roundtrip" in { forAll { (n: Short) => whenever(n >= 0) { roundtrip(ushortL(15), n) } } } }
+
+  "the short codecs" should {
+    "support endianess correctly" in {
+      forAll { (n: Short) =>
+        val bigEndian = short16.encode(n).toOption.err("big").toByteVector
+        val littleEndian = short16L.encode(n).toOption.err("little").toByteVector
+        littleEndian shouldBe bigEndian.reverse
+      }
+      check(0, 8191) { (n: Short) =>
+        whenever(n >= 0 && n <= 8191) {
+          val bigEndian = ushort(13).encode(n).valueOr(sys.error)
+          val littleEndian = ushortL(13).encode(n).valueOr(sys.error).toByteVector
+          val flipped = BitVector(littleEndian.last).take(5) ++ littleEndian.init.reverse.toBitVector
+          flipped shouldBe bigEndian
+        }
+      }
+    }
+
+    "return an error when value to encode is out of legal range" in {
+      short(15).encode(Short.MaxValue) shouldBe \/.left("32767 is greater than maximum value 16383 for 15-bit signed short")
+      short(15).encode(Short.MinValue) shouldBe \/.left("-32768 is less than minimum value -16384 for 15-bit signed short")
+      ushort(15).encode(-1) shouldBe \/.left("-1 is less than minimum value 0 for 15-bit unsigned short")
+    }
+
+    "return an error when decoding with too few bits" in {
+      short16.decode(BitVector.low(8)) shouldBe \/.left("cannot acquire 16 bits from a vector that contains 8 bits")
+    }
+  }
+}
