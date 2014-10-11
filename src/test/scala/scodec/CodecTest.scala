8265433c2f732175c4ed63fe6fd06dc7f9ae8bed
Michael Pilquist <mpilquist@gmail.com>
2014-10-11 13:27:13 -0400

    Backport labelled generic support

--- src/test/scala/scodec/CodecTest.scala
+++ src/test/scala/scodec/CodecTest.scala
@@ -127,7 +127,7 @@ class CodecTest extends CodecSuite {
     }
     "include field names in case class codecs" in {
       implicit val (i, s) = (uint8, variableSizeBytes(uint16, utf8))
-      Codec.auto[Foo].encode(Foo(1, 256, "Hello")) shouldBe left(Err("256 is greater than maximum value 255 for 8-bit unsigned integer").pushContext("y"))
+      Codec.auto[Foo].encode(Foo(1, 256, "Hello")) shouldBe left("y: 256 is greater than maximum value 255 for 8-bit unsigned integer")
     }
   }
 }
