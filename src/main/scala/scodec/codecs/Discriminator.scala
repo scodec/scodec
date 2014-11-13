7b238d16da236632f8f353a424cf0ea0acec2d73
Michael Pilquist <mpilquist@gmail.com>
2014-11-13 11:38:18 -0500

    Update docs for Discriminator and Discriminated

--- src/main/scala/scodec/codecs/Discriminator.scala
+++ src/main/scala/scodec/codecs/Discriminator.scala
@@ -3,21 +3,29 @@ package codecs
 
 /**
  * Wrapper class that discriminates type `Y` in context of type `X` with a
- * value of type `B`.
+ * value of type `D`.
  *
  * For example, consider a sealed trait and its subclasses. Each subclass
  * could be assigned an integer by defining a `Discriminator[Parent, Subclass, Int]`
  * for each.
+ *
+ * @tparam X context in which type `Y` is discriminated from other types
+ * @tparam Y type that is differentiated from other types by values of `D`
+ * @tparam D value type that discriminates values of `Y` from other types in context of `X`
  */
 case class Discriminator[X, Y, D](value: D)
 
 /**
- * Wrapper class that indicates subtypes of `Y` are discriminated by type `D`
+ * Wrapper class that indicates subtypes of `X` are discriminated by type `D`
  * using the supplied `Codec[D]`.
  *
  * For example, an implicit `Discriminated` value can be defined in the companion
  * of a sealed trait, along with implicit `Discriminator` values in each subtype
  * companion. Given such implicits, a codec for the trait can be automatically
- * created using `Codec.derive[Y]`.
+ * created using `Codec.derive[X]`.
+ *
+ * @tparam X type that can be discriminated by discriminator values of type `D`
+ * @tparam D value type that discriminates `Y` in context of `X` from other types
+ *           using discriminators of `D` for some type `Y`
  */
-case class Discriminated[Y, D](codec: Codec[D])
+case class Discriminated[X, D](codec: Codec[D])
