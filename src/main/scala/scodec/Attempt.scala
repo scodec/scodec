1d14291575891f67c136d8933461219e6c27099a
Michael Pilquist <mpilquist@gmail.com>
2015-01-09 22:21:00 -0500

    Add flatten, getOrElse, orElse, recover, and recoverWith to Attempt

--- src/main/scala/scodec/Attempt.scala
+++ src/main/scala/scodec/Attempt.scala
@@ -24,8 +24,31 @@ sealed abstract class Attempt[+A] {
   /** Maps the supplied function over the successful value, if present. */
   def flatMap[B](f: A => Attempt[B]): Attempt[B]
 
+  /** Converts an `Attempt[Attempt[X]]` in to an `Attempt[X]`. */
+  def flatten[B](implicit ev: A <:< Attempt[B]): Attempt[B]
+
   /** Transforms this attempt to a value of type `B` using the supplied functions. */
   def fold[B](ifFailure: Err => B, ifSuccessful: A => B): B
+
+  /** Returns the sucessful value if successful, otherwise the supplied default. */
+  def getOrElse[B >: A](default: => B): B
+
+  /** Returns this attempt if successful, otherwise the fallback attempt. */
+  def orElse[B >: A](fallback: => Attempt[B]): Attempt[B]
+
+  /**
+   * If this attempt is a failure, and the supplied partial function is defined for the cause of the failure,
+   * a successful attempt is returned. If this attempt is successful or the supplied function is not defined
+   * for the cause of the failure, this attempt is returned unmodified.
+   */
+  def recover[B >: A](f: PartialFunction[Err, B]): Attempt[B]
+
+  /**
+   * If this attempt is a failure, and the supplied partial function is defined for the cause of the failure,
+   * the result of applying that function is returned. If this attempt is successful or the supplied
+   * function is not defined for the cause of the failure, this attempt is returned unmodified.
+   */
+  def recoverWith[B >: A](f: PartialFunction[Err, Attempt[B]]): Attempt[B]
 
   /** Returns the successful value if present; otherwise throws an `IllegalArgumentException`. */
   def require: A
@@ -69,7 +92,12 @@ object Attempt {
     def map[B](f: A => B): Attempt[B] = Successful(f(value))
     def mapErr(f: Err => Err): Attempt[A] = this
     def flatMap[B](f: A => Attempt[B]): Attempt[B] = f(value)
+    def flatten[B](implicit ev: A <:< Attempt[B]): Attempt[B] = value
     def fold[B](ifFailure: Err => B, ifSuccessful: A => B): B = ifSuccessful(value)
+    def getOrElse[B >: A](default: => B): B = value
+    def orElse[B >: A](fallback: => Attempt[B]) = this
+    def recover[B >: A](f: PartialFunction[Err, B]): Attempt[B] = this
+    def recoverWith[B >: A](f: PartialFunction[Err, Attempt[B]]): Attempt[B] = this
     def require: A = value
     def isSuccessful: Boolean = true
     def toOption: Some[A] = Some(value)
@@ -81,7 +109,16 @@ object Attempt {
     def map[B](f: Nothing => B): Attempt[B] = this
     def mapErr(f: Err => Err): Attempt[Nothing] = Failure(f(cause))
     def flatMap[B](f: Nothing => Attempt[B]): Attempt[B] = this
+    def flatten[B](implicit ev: Nothing <:< Attempt[B]): Attempt[B] = this
     def fold[B](ifFailure: Err => B, ifSuccessful: Nothing => B): B = ifFailure(cause)
+    def getOrElse[B >: Nothing](default: => B): B = default
+    def orElse[B >: Nothing](fallback: => Attempt[B]) = fallback
+    def recover[B >: Nothing](f: PartialFunction[Err, B]): Attempt[B] =
+      if (f.isDefinedAt(cause)) Attempt.successful(f(cause))
+      else this
+    def recoverWith[B >: Nothing](f: PartialFunction[Err, Attempt[B]]): Attempt[B] =
+      if (f.isDefinedAt(cause)) f(cause)
+      else this
     def require: Nothing = throw new IllegalArgumentException(cause.messageWithContext)
     def isSuccessful: Boolean = false
     def toOption: None.type = None
