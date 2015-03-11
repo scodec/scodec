119564af0da78baf5016faaf7c59ebd3f6806bdb
Michael Pilquist <mpilquist@gmail.com>
2015-03-11 17:49:46 -0400

    Fixes #47

--- src/main/scala/scodec/codecs/package.scala
+++ src/main/scala/scodec/codecs/package.scala
@@ -5,6 +5,7 @@ import scala.language.implicitConversions
 import java.nio.charset.Charset
 import java.security.cert.{ Certificate, X509Certificate }
 import java.util.UUID
+import java.util.zip.Deflater
 
 import scodec.bits.{ BitVector, ByteOrdering, ByteVector }
 
@@ -948,6 +949,20 @@ package object codecs {
    * @group combinators
    */
   def fail[A](encErr: Err, decErr: Err): Codec[A] = new FailCodec[A](encErr, decErr)
+
+  /**
+   * Codec that compresses the results of encoding with the specified codec and decompresses prior to decoding with the specified codec.
+   *
+   * Compression is performed using ZLIB. There are a number of defaulted parameters that control compression specifics.
+   *
+   * @param level compression level, 0-9, with 0 disabling compression and 9 being highest level of compression -- see `java.util.zip.Deflater` for details
+   * @param strategy compression strategy -- see `java.util.zip.Deflater` for details
+   * @param nowrap if true, ZLIB header and checksum will not be used
+   * @param chunkSize buffer size, in bytes, to use when compressing
+   * @group combinators
+   */
+  def zlib[A](codec: Codec[A], level: Int = Deflater.DEFAULT_COMPRESSION, strategy: Int = Deflater.DEFAULT_STRATEGY, nowrap: Boolean = false, chunkSize: Int = 4096): Codec[A] =
+    new ZlibCodec(codec, level, strategy, nowrap, chunkSize)
 
   /**
    * Codec that encrypts and decrypts using a `javax.crypto.Cipher`.
