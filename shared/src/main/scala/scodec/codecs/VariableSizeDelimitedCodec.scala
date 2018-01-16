ba5f8628126e335197d93c03871210601450dca3
Jarlakxen <facundo.m.viale@gmail.com>
2018-01-16 09:45:46 -0300

    Add variable size delimited codec

---
+++ shared/src/main/scala/scodec/codecs/VariableSizeDelimitedCodec.scala
@@ -0,0 +1,46 @@
+package scodec
+package codecs
+
+import scodec.bits.BitVector
+
+private[codecs] final class VariableSizeDelimitedCodec[A](delimiterCodec: Codec[Unit], valueCodec: Codec[A], multipleValueSize: Long = 0L) extends Codec[A] {
+
+  val delimiter = delimiterCodec.encode(()).require
+  val segmentSize = valueCodec.sizeBound.exact.getOrElse(multipleValueSize)
+
+  require(segmentSize > 0, "valueCodec must have an exact sizeBound or you need to specify multipleValueSize")
+
+  require(delimiterCodec.sizeBound.lowerBound >= segmentSize, "delimiterCodec cannot be smaller than the sizeBound of the valueCodec")
+
+  def sizeBound = delimiterCodec.sizeBound.atLeast
+
+  override def encode(a: A) = for {
+    encA <- valueCodec.encode(a)
+    encDel <- delimiterCodec.encode(())
+  } yield encA ++ encDel
+
+  override def decode(buffer: BitVector) = {
+    val index = findDelimiterIndex(buffer) 
+    if (index != -1) {
+      val valueBuffer = buffer.take(index)
+      val remainder = buffer.drop(index + delimiter.size)
+      println(s"VariableSizeDelimitedCodec.decode: [${delimiter.size}] $index [${valueBuffer.size}]$valueBuffer [${remainder.size}]$remainder")
+      valueCodec.decode(valueBuffer).map(decodeResult => decodeResult.mapRemainder(_ ++ remainder) )
+    } else {
+      Attempt.failure(Err(s"expected delimiter $delimiterCodec"))
+    }
+  }
+
+  private def findDelimiterIndex(buffer: BitVector): Long = {
+    var offset = 0L
+    do {
+      if (buffer.drop(offset).startsWith(delimiter)) {
+        return offset
+      }
+      offset += segmentSize
+    } while(offset < buffer.size)
+    -1
+  }
+
+  override def toString = s"VariableSizeDelimited($delimiterCodec, $valueCodec, $multipleValueSize)"
+}
