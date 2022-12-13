202bdbe1b6e3dde9e2686c7cdb5d119c9dce19f5
a.artigao <a.artigao@stuart.com>
2022-12-13 09:31:16 +0100

    More efficient long conversion for negative ints in `VarIntCodec`
    Two bitwise operations can be squashed into a single one.

--- shared/src/main/scala/scodec/codecs/VarIntCodec.scala
+++ shared/src/main/scala/scodec/codecs/VarIntCodec.scala
@@ -48,9 +48,6 @@ private[codecs] final class VarIntCodec(ordering: ByteOrdering) extends Codec[In
   override def toString = "variable-length integer"
 
 private object VarIntCodec:
-  private val NegativeIntSignBit = Int.MaxValue.toLong + 1L
-
   // toLong left-pads with `1` if the int is negative which cannot be encoded by
   // the VarLongCodec. This pads negative ints with `0` instead.
-  private val toPositiveLong = (i: Int) =>
-    if i >= 0 then i.toLong else (i & Int.MaxValue) | NegativeIntSignBit
+  private val toPositiveLong = (i: Int) => if i >= 0 then i.toLong else i & 0x0ffffffffL
