725250305a938d0f1e5d419af53e33cc25ee673b
nikiforo <anikiforov@delimobil.ru>
2023-05-02 13:54:44 +0400

    issue-463 - fix constantLenient

--- shared/src/main/scala/scodec/codecs/ConstantCodec.scala
+++ shared/src/main/scala/scodec/codecs/ConstantCodec.scala
@@ -42,12 +42,10 @@ private[codecs] final class ConstantCodec(constant: BitVector, validate: Boolean
     Attempt.successful(constant)
 
   override def decode(buffer: BitVector) =
-    if validate then
-      buffer.acquire(constant.size) match
-        case Left(_) => Attempt.failure(Err.insufficientBits(constant.size, buffer.size))
-        case Right(b) =>
-          if b == constant then Attempt.successful(DecodeResult((), buffer.drop(constant.size)))
-          else Attempt.failure(Err(s"expected constant $constant but got $b"))
-    else Attempt.successful(DecodeResult((), buffer.drop(constant.size)))
+    buffer.acquire(constant.size) match
+      case Left(_) => Attempt.failure(Err.insufficientBits(constant.size, buffer.size))
+      case Right(b) =>
+        if !validate || b == constant then Attempt.successful(DecodeResult((), buffer.drop(constant.size)))
+        else Attempt.failure(Err(s"expected constant $constant but got $b"))
 
   override def toString = s"constant($constant)"
