11b7123d9d1bf76e908b9b6fab60a7530eea7b7f
mpilquist <mpilquist@gmail.com>
2023-10-23 08:19:40 -0400

    Scalafmt

--- shared/src/main/scala/scodec/codecs/ChecksumPrependCodec.scala
+++ shared/src/main/scala/scodec/codecs/ChecksumPrependCodec.scala
@@ -44,15 +44,20 @@ private[codecs] final class ChecksumPrependCodec[V](
   private val codec = bits(bitSize) :: target
 
   def decode(bits: BitVector): Attempt[DecodeResult[V]] =
-    codec.decode(bits).flatMap:
-      case DecodeResult((expectedCrc, value), remainder) =>
-        val valueBits = bits.drop(bitSize).dropRight(remainder.size)
-        val actualCrc = compute(framing(valueBits))
-        if !validate || expectedCrc == actualCrc then Attempt.successful(DecodeResult(value, remainder))
-        else Attempt.failure(ChecksumMismatch(valueBits, expectedCrc, actualCrc))
+    codec
+      .decode(bits)
+      .flatMap:
+        case DecodeResult((expectedCrc, value), remainder) =>
+          val valueBits = bits.drop(bitSize).dropRight(remainder.size)
+          val actualCrc = compute(framing(valueBits))
+          if !validate || expectedCrc == actualCrc then
+            Attempt.successful(DecodeResult(value, remainder))
+          else Attempt.failure(ChecksumMismatch(valueBits, expectedCrc, actualCrc))
 
   def encode(value: V): Attempt[BitVector] =
-    target.encode(value).map: bits =>
-      compute(framing(bits)) ++ bits
+    target
+      .encode(value)
+      .map: bits =>
+        compute(framing(bits)) ++ bits
 
   def sizeBound: SizeBound = target.sizeBound + SizeBound.exact(bitSize)
